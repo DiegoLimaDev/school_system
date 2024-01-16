7cf0792ac77e4bcece2acf2678a5eee3ee89a0df
Diego Lima <xxdiegolsxx@gmail.com>
2024-01-16 11:33:48 -0300

    [feat]new modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { LoginModule } from './app/core/login/login.module';
+import { AdminModule } from './app/api/admin/admin.module';
+import { RegisterModule } from './app/core/register/register.module';
+import { AuthModule } from './app/core/auth/auth.module';
+import { SchoolModule } from './app/api/school/school.module';
+import { TeachersModule } from './app/api/teachers/teachers.module';
+import { StudentModule } from './app/api/student/student.module';
 
 @Module({
   imports: [
@@ -12,10 +19,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: 'postgres',
       password: '1234',
       database: 'consulticom-db',
-      entities: [],
+      entities: [
+        `\.entity\.ts$
+      `,
+      ],
       synchronize: true,
       autoLoadEntities: true,
     }),
+    LoginModule,
+    AdminModule,
+    RegisterModule,
+    AuthModule,
+    SchoolModule,
+    TeachersModule,
+    StudentModule,
   ],
   controllers: [AppController],
   providers: [AppService],
