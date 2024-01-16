35a7e4e110d9e6fd85b417e85c71a3eb13b23db3
Diego Lima <xxdiegolsxx@gmail.com>
2024-01-16 11:59:49 -0300

    [feat]register teacher component

---
+++ frontend/src/app/presentation/components/RegisterTeacher/RegisterTeacher.tsx
@@ -0,0 +1,93 @@
+import {
+  Box,
+  Button,
+  MenuItem,
+  OutlinedInput,
+  Select,
+  SelectChangeEvent,
+  Typography,
+} from '@mui/material';
+import { theme } from '../../../../styles/theme';
+import { useHttp } from '../../../infra/useHttp/useHttp';
+import { useEffect, useState } from 'react';
+import { TeacherDomain } from '../../../domain/teacher.domain';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { useAppDispatch, useAppSelector } from '../../../application/hooks';
+import { getSchools } from '../../../application/getSchools/getSchools.slice';
+
+const RegisterTeacher = () => {
+  const url = 'http://localhost:8000/';
+  const [schoolName, setSchoolName] = useState('');
+  const { register, handleSubmit } = useForm<TeacherDomain>();
+  const { entities } = useAppSelector((state) => state.getSchoolsReducer);
+  const dispatch = useAppDispatch();
+
+  //função assíncrona para disparar o get nas escolas
+  const getData = async () => {
+    dispatch(getSchools());
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setSchoolName(event.target.value);
+  };
+
+  //função post de criação de professores
+  const submitTeacher: SubmitHandler<TeacherDomain> = async (data) => {
+    useHttp
+      .post(`${url}register/teacher`, {
+        cpf: data.cpf,
+        name: data.name,
+        schoolName: schoolName,
+      })
+      .then(() => window.location.reload())
+      .catch((err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+  };
+
+  return (
+    <>
+      <form>
+        <Box
+          display={'flex'}
+          flexDirection={'column'}
+          width={'20rem'}
+          m={'5rem auto 0 auto'}
+          justifyContent={'space-between'}
+          height={'25rem'}
+        >
+          <Typography fontSize={theme.sizes.medium}>Criar Professor</Typography>
+          <OutlinedInput
+            notched
+            placeholder={'CPF'}
+            {...register('cpf', { required: true })}
+            inputProps={{ maxLength: 11 }}
+          />
+          <OutlinedInput notched placeholder={'Nome'} {...register('name')} />
+          <Select notched value={schoolName} onChange={handleChange}>
+            {entities.map((school) => {
+              return (
+                <MenuItem value={school.schoolName} key={school.id}>
+                  {school.schoolName}
+                </MenuItem>
+              );
+            })}
+          </Select>
+          <Button variant="contained" onClick={handleSubmit(submitTeacher)}>
+            <Typography fontSize={theme.sizes.small}>
+              Criar Professor
+            </Typography>
+          </Button>
+        </Box>
+      </form>
+    </>
+  );
+};
+
+export default RegisterTeacher;
