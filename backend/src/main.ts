aaa1ef69c9474f98674917cca110d5df5262c22b
Diego Lima <xxdiegolsxx@gmail.com>
2024-01-16 11:38:17 -0300

    [refactor]removed prefix and changed port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,9 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
-  app.setGlobalPrefix('api/v1');
   app.enableCors();
 
-  await app.listen(3000);
+  await app.listen(8000);
 }
 bootstrap();
