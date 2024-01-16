73376518d031782b6e1f8325e8eaca7c776293db
Diego Lima <xxdiegolsxx@gmail.com>
2024-01-16 11:59:22 -0300

    [feat]useHttp class

--- frontend/src/app/infra/useHttp/useHttp.ts
+++ frontend/src/app/infra/useHttp/useHttp.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+//class para consumir e transportar os dados da api
 export class useHttp {
-  static async get(url: string): Promise<unknown> {
+  static async get(url: string) {
     const data = await axios.get(url).then((res) => res.data);
 
     return data;
@@ -10,4 +11,9 @@ export class useHttp {
   static async post(url: string, obj: unknown) {
     return await axios.post(url, obj);
   }
+
+  static async patch(url: string, obj: unknown) {
+    return await axios.patch(url, obj);
+  }
 }
+('');
