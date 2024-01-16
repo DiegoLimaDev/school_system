11d68e6ed905f2e80d19025a79f36497d7df2ca1
Diego Lima <xxdiegolsxx@gmail.com>
2024-01-16 11:57:16 -0300

    [docs]documentação

--- frontend/src/app/application/hooks.ts
+++ frontend/src/app/application/hooks.ts
@@ -1,6 +1,8 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from './store';
 
+//função para disparar as mudanças de estado do redux
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 
+//função para selecionas os estados do redux
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
