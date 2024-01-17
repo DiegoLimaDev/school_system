7919251bae9a55541cf791f42a859fa4fc2efe9c
Diego Lima <xxdiegolsxx@gmail.com>
2024-01-17 11:55:37 -0300

    [refactor]

--- frontend/src/app/presentation/pages/Admin/Admin.tsx
+++ frontend/src/app/presentation/pages/Admin/Admin.tsx
@@ -1,59 +1,27 @@
-import { Box, Button, OutlinedInput, Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { theme } from '../../../../styles/theme';
-import { useHttp } from '../../../infra/useHttp/useHttp';
-import { SubmitHandler, useForm } from 'react-hook-form';
-import { SchoolDomain } from '../../../domain/school.domain';
 import RegisterTeacher from '../../components/RegisterTeacher/RegisterTeacher';
+import LeaveButton from '../../components/LeaveButton/LeaveButton';
+import RegisterSchool from '../../components/RegisterSchool/RegisterSchool';
 
 const AdminPage = () => {
-  const url = 'http://localhost:8000/';
-  const { register, handleSubmit } = useForm<SchoolDomain>();
-
-  //método post para registrar a escola
-  const submitSchool: SubmitHandler<SchoolDomain> = async (
-    data: SchoolDomain,
-  ) => {
-    useHttp
-      .post(`${url}register/school`, {
-        schoolName: data.schoolName,
-        schoolAddress: data.schoolAddress,
-      })
-      .then(() => window.location.reload())
-      .catch((err) => {
-        if (err) {
-          console.log('bad request');
-        }
-      });
-  };
-
   return (
     <>
-      <form>
+      <Box>
         <Box
           display={'flex'}
-          flexDirection={'column'}
-          width={'20rem'}
           m={'5rem auto 0 auto'}
-          justifyContent={'space-between'}
-          height={'25rem'}
+          width={'fit-content'}
+          flexDirection={'column'}
         >
-          <Typography fontSize={theme.sizes.medium}>Criar Escola</Typography>
-          <OutlinedInput
-            notched
-            placeholder={'Nome da escola'}
-            {...register('schoolName')}
-          />
-          <OutlinedInput
-            notched
-            placeholder={'Endereço'}
-            {...register('schoolAddress')}
-          />
-          <Button variant="contained" onClick={handleSubmit(submitSchool)}>
-            <Typography fontSize={theme.sizes.small}>Criar escola</Typography>
-          </Button>
+          <Typography fontSize={theme.sizes.medium} textAlign={'left'}>
+            Admin
+          </Typography>
         </Box>
-      </form>
+      </Box>
+      <RegisterSchool />
       <RegisterTeacher />
+      <LeaveButton />
     </>
   );
 };
