1ad7aef319a101cd557706c1fcd09c2607344ff6
Diego Lima <xxdiegolsxx@gmail.com>
2024-01-17 11:54:38 -0300

    [feat]RegisterSchool component

---
+++ frontend/src/app/presentation/components/RegisterSchool/RegisterSchool.tsx
@@ -0,0 +1,58 @@
+import { Box, OutlinedInput, Typography } from '@mui/material';
+import MyButton from '../Button/MyButton';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { SchoolDomain } from '../../../domain/school.domain';
+import { useHttp } from '../../../infra/useHttp/useHttp';
+import { theme } from '../../../../styles/theme';
+
+const RegisterSchool = () => {
+  const url = 'http://localhost:8000/';
+  const { register, handleSubmit } = useForm<SchoolDomain>();
+
+  //método post para registrar a escola
+  const submitSchool: SubmitHandler<SchoolDomain> = async (
+    data: SchoolDomain,
+  ) => {
+    useHttp
+      .post(`${url}register/school`, {
+        schoolName: data.schoolName,
+        schoolAddress: data.schoolAddress,
+      })
+      .then(() => window.location.reload())
+      .catch((err) => {
+        if (err) {
+          console.log('bad request');
+        }
+      });
+  };
+
+  return (
+    <form>
+      <Box
+        display={'flex'}
+        flexDirection={'column'}
+        width={'20rem'}
+        m={'5rem auto 0 auto'}
+        justifyContent={'space-between'}
+        height={'25rem'}
+      >
+        <Typography fontSize={theme.sizes.medium}>Criar Escola</Typography>
+        <OutlinedInput
+          notched
+          placeholder={'Nome da escola'}
+          {...register('schoolName')}
+        />
+        <OutlinedInput
+          notched
+          placeholder={'Endereço'}
+          {...register('schoolAddress')}
+        />
+        <MyButton variant="contained" onClick={handleSubmit(submitSchool)}>
+          CRIAR ESCOLA
+        </MyButton>
+      </Box>
+    </form>
+  );
+};
+
+export default RegisterSchool;
