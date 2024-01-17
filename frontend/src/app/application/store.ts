52075ddbdb6f3247723bac426c23cf98732d7f53
Diego Lima <xxdiegolsxx@gmail.com>
2024-01-17 11:53:50 -0300

    [refactor]

--- frontend/src/app/application/store.ts
+++ frontend/src/app/application/store.ts
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from './combineReducers';
 import {
   persistStore,
   persistReducer,
@@ -11,6 +10,7 @@ import {
   REGISTER,
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
+import { rootReducer } from './combineReducers';
 
 const persistConfig = {
   key: 'root',
