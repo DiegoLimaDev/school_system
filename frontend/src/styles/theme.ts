a0987cd73c0e1b8a1235f5abd0e85c3091634c0e
Diego Lima <xxdiegolsxx@gmail.com>
2024-01-17 11:56:04 -0300

    [style]new colors

--- frontend/src/styles/theme.ts
+++ frontend/src/styles/theme.ts
@@ -1,7 +1,17 @@
 export const theme = {
   sizes: {
+    verySmall: '1.2rem',
     small: '1.5rem',
     medium: '2rem',
     medium2: '2.5rem',
+  },
+  colors: {
+    primary: '#012340',
+    secondary: '#025959',
+    third: '#027333',
+    fourth: '#03A63C',
+    fifth: '#04D939',
+    black: '#00040D',
+    white: '#FFFFF0',
   },
 };
