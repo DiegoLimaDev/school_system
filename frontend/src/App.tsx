dc94b02a0dc8c7e86fb2a3244019acdae97fec7d
Diego Lima <xxdiegolsxx@gmail.com>
2024-01-17 11:53:04 -0300

    [style]defined minHeight

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,16 +3,23 @@ import Home from './app/presentation/pages/Home/Home';
 import { GlobalStyle } from './styles/globalStyles';
 import AdminPage from './app/presentation/pages/Admin/Admin';
 import TeacherPage from './app/presentation/pages/Teacher/Teacher';
+import Header from './app/presentation/layout/Header';
+import Footer from './app/presentation/layout/Footer';
+import { Box } from '@mui/material';
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/admin" element={<AdminPage />} />
-        <Route path="/teacher" element={<TeacherPage />} />
-      </Routes>
+      <Header />
+      <Box minHeight={'90dvh'}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/admin" element={<AdminPage />} />
+          <Route path="/teacher" element={<TeacherPage />} />
+        </Routes>
+      </Box>
+      <Footer />
     </>
   );
 }
