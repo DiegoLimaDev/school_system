e430078c2dcd2d363bbc6683f4aa7c48185156f4
Diego Lima <xxdiegolsxx@gmail.com>
2024-01-18 10:20:59 -0300

    [feat]admin seeder

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
+import { AdminService } from './app/api/admin/admin.service';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'The server is online';
+export class AppService implements OnApplicationBootstrap {
+  constructor(private adminService: AdminService) {}
+
+  //Verifica se há um usuário admin padrão, caso não cria o usuário
+  async onApplicationBootstrap() {
+    const admin = await this.adminService.findOne('00000000000');
+
+    if (!admin) {
+      await this.adminService.create({
+        cpf: '00000000000',
+        password: '12345',
+        usertype: 0,
+      });
+    }
   }
 }
